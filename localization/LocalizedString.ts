8611acaf68afe3bc239f764bd019d1641b53a150
fxMem <faarseer@ya.ru>
2020-06-19 20:17:25 +0300

    added localized strings ability to have template parameters

--- localization/LocalizedString.ts
+++ localization/LocalizedString.ts
@@ -1,8 +1,19 @@
 import { defaulLanguage, getCurrentLanguage } from "./Language";
-import { localizationData } from "./LocalizationConfig";
+import { LocalizationData, LocalizationConfig } from "./Localization";
 
-export type LocalizedString = () => string;
+export type LocalizedString = (...parameters: string[]) => string;
 
-export function localizedStringBuilder(name: localizationData): LocalizedString {
-    return () => name[getCurrentLanguage()] ?? name[defaulLanguage];
+export function localizedStringBuilder(name: LocalizationData): LocalizedString {
+    return (...parameters: string[]) => format(name[getCurrentLanguage()] ?? name[defaulLanguage], parameters);
 }
+
+// trimmed version of https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format/4673436#4673436
+function format(rawString: string, parameters: string[]) {
+    return rawString.replace(/{(\d+)}/g, function (match, number) {
+        return typeof parameters[number] != 'undefined'
+            ? parameters[number]
+            : match;
+    });
+};
+
+
