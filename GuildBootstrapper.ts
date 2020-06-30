79dea4e64d1f3a850e653af315856b7c3db3e631
fxMem <faarseer@ya.ru>
2020-07-01 02:19:53 +0300

    update per-guild initialization mechanism

--- GuildBootstrapper.ts
+++ GuildBootstrapper.ts
@@ -1,5 +1,6 @@
 import { GuildContext } from "./discord/GuildContext";
-import { createLocalLogScope, exitLogScope } from "./log/LogScopes";
+import { createLocalLogScope } from "./log/LogScopes";
+import { PromiseSource, createPromiseSource } from "./common/PromiseSource";
 
 export type RequiresGuildInitialization = {
     initializeGuild: GuildInitializationCallback;
@@ -7,65 +8,106 @@ export type RequiresGuildInitialization = {
 
 export type GuildInitializationCallback = (context: GuildContext) => Promise<void>;
 
-let initializationComplete = false;
-let initializatoinError = null;
-const subscribers: GuildInitializationCallback[] = [];
+export type GuildInitializer = RequiresGuildInitialization | GuildInitializationCallback;
+let initializationResults: { [guildId: string]: { error?: any } } = {};
+
+type InitializationData = {
+    error?: string;
+    callback: GuildInitializationCallback;
+    promises: { [guildId: string]: PromiseSource<void> };
+}
+const subscribers: InitializationData[] = [];
 
 function isRequiresGuildInitialization(argument: RequiresGuildInitialization | GuildInitializationCallback): argument is RequiresGuildInitialization {
     return !!(argument as any).initializeGuild;
 }
 
-export function registerForGuildInitialization<T extends RequiresGuildInitialization | GuildInitializationCallback>(subscriber: T): T {
-    if (isRequiresGuildInitialization(subscriber)) {
-        subscribers.push(subscriber.initializeGuild.bind(subscriber));
-    }
-    else {
-        subscribers.push(subscriber as any);
-    }
+export type GuildInitializerResult<T> = {
+    // Returned promise is resolved when iniitializer had run for the guild
+    ensure(guildInfo: string | GuildContext): Promise<T>,
 
-    return subscriber;
+    // Registers dependent initializer, which will run after this one
+    chain<TNext extends GuildInitializer>(subscriber: TNext): GuildInitializerResult<TNext>;
 }
 
+export function registerForGuildInitialization<T extends GuildInitializer>(subscriber: T): GuildInitializerResult<T> {
+    const callback = isRequiresGuildInitialization(subscriber)
+        ? subscriber.initializeGuild.bind(subscriber)
+        : subscriber;
+
+    const data: InitializationData = {
+        callback,
+        promises: {}
+    };
+
+    subscribers.push(data);
+    return {
+        ensure: (guildId: string) => getInitializationDataForGuild(guildId, data).promise.then(() => subscriber),
+        chain: <TNext extends GuildInitializer>(initializer: TNext) => {
+            // Invoking the dependant initializer after this one we ensure that dependant is added to subscribers array after this one,
+            // hence preservicng the rigth order
+            return registerForGuildInitialization(initializer);
+        }
+    };
+}
+
+function getInitializationDataForGuild(guildInfo: string | GuildContext, subscriber: InitializationData) {
+    const guildId = typeof guildInfo === 'string' ? guildInfo : guildInfo.guild.id;
+    return subscriber.promises[guildId] || (subscriber.promises[guildId] = createPromiseSource<void>());
+}
+
+
 export function runGuildInitializers(context: GuildContext): Promise<void> {
-    if (initializationComplete) {
-        return initializatoinError ? Promise.reject(initializatoinError) : Promise.resolve();
+    const guildId = context.guild.id;
+    const previousResults = initializationResults[guildId];
+    if (previousResults) {
+        return previousResults.error ? Promise.reject(previousResults.error) : Promise.resolve();
     }
 
     const log = createLocalLogScope('Bootstrapper');
-
     return new Promise((resolve, reject) => {
         let currentSubscriberIndex = 0;
-
         initializeNextInChain(getNextSubscriber());
 
-        function initializeNextInChain(subscriber: GuildInitializationCallback) {
+        function initializeNextInChain(subscriber: InitializationData) {
             if (!subscriber) {
+                // We've run out of initializers
                 initializationFinished();
                 return;
             }
 
-            subscriber(context).then(() => {
+            const promiseSourceForGuild = getInitializationDataForGuild(guildId, subscriber);
+            if (promiseSourceForGuild.done) {
+                // Initializer for this guild had already been run before. This shouldn't normally happen
+                // as we use initializationResults to track such cases
+                throw new Error(`Attemt to run initializer for guild ${guildId} which already was executed!`);
+            }
+
+            subscriber.callback(context).then(() => {
                 initializeNextInChain(getNextSubscriber());
             }).catch((e) => {
-                initializatoinError = e;
+                promiseSourceForGuild.reject(e);
                 initializationFinished();
             });
+
+
+            function initializationFinished() {
+                const error = promiseSourceForGuild.error;
+                initializationResults[guildId] = {};
+                if (error) {
+                    log.fatal(`Guild [${context.guild.name}] initialization failed! Error: ${error}`);
+                    initializationResults[guildId].error = error;
+                    reject(error);
+                }
+                else {
+                    log.info(`Guild [${context.guild.name}] initialization completed!`);
+                    resolve();
+                }
+            }
         }
 
         function getNextSubscriber() {
             return subscribers[currentSubscriberIndex++];
-        }
-
-        function initializationFinished() {
-            initializationComplete = true;
-            if (initializatoinError) {
-                log.fatal(`Guild [${context.guild.name}] initialization failed! Error: ${initializatoinError}`);
-                reject(initializatoinError);
-            }
-            else {
-                log.info(`Guild [${context.guild.name}] initialization completed!`);
-                resolve();
-            }
         }
     });
 }
