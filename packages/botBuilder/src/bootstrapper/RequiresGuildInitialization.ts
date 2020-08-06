396334a60859280d44f99975db7df284e5c7fabd
fxMem <faarseer@ya.ru>
2020-08-07 02:33:34 +0300

    add func to register multiple initializers

--- packages/botBuilder/src/bootstrapper/RequiresGuildInitialization.ts
+++ packages/botBuilder/src/bootstrapper/RequiresGuildInitialization.ts
@@ -1,5 +1,5 @@
 import { GuildContext } from "../discord/GuildContext";
-import { registerForGuildInitialization } from "./GuildBootstrapper";
+import { registerForGuildInitialization, GuildInitializerResult } from "./GuildBootstrapper";
 
 export type RequiresGuildInitialization = {
     context: GuildContext;
@@ -10,6 +10,20 @@ export type GuildInitializationCallback<T> = (context: GuildContext) => Promise<
 
 export function registerClassInitializer<T extends RequiresGuildInitialization>(factory: () => T, name: string) {
     return registerForGuildInitialization(buildClassInitializer(factory), name);
+}
+
+export function registerClassInitializers<T extends RequiresGuildInitialization>(factoris: { factory: () => T, name: string }[]) {
+    let results: GuildInitializerResult<T>[] = [];
+    let previous: GuildInitializerResult<T> = null;
+    for (const item of factoris) {
+        previous = previous === null 
+            ? registerClassInitializer(item.factory, item.name) 
+            : previous.chain(buildClassInitializer(item.factory), item.name);
+
+        results.push(previous);
+    }
+
+    return results;
 }
 
 export function buildClassInitializer<T extends RequiresGuildInitialization>(factory: () => T): GuildInitializationCallback<T> {
