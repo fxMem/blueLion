71ec905bf27c3da8e3b62fc62ab0a484fbf72b9b
fxMem <faarseer@ya.ru>
2020-09-19 16:04:00 +0300

    add random utility

--- packages/botBuilder/src/common/Random.ts
+++ packages/botBuilder/src/common/Random.ts
@@ -4,4 +4,8 @@ export function getRandomIntFromInterval(from: number, to: number): number {
     }
 
     return from + Math.trunc(Math.random() * (to + 1 - from));
-}
+};
+
+export function getRandomValueFromArray<T>(array: T[]): T {
+    return array[getRandomIntFromInterval(0, array.length - 1)];
+};
