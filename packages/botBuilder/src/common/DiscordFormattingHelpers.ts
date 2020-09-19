5cad0f838e549d580aa7d8c6b84ec62baae3d2e9
fxMem <faarseer@ya.ru>
2020-09-19 16:05:43 +0300

    add formatting helpers

--- packages/botBuilder/src/common/DiscordFormattingHelpers.ts
+++ packages/botBuilder/src/common/DiscordFormattingHelpers.ts
@@ -9,3 +9,44 @@ export function italic(message: string) {
 export function code(message: string) {
     return `\`${message}\``;
 }
+
+export function cssRed(message: string) {
+    return [`[${message}]`];
+}
+
+export function asciiRed(message: string) {
+    return [`[${message}]`];
+}
+
+export function cssGreen(message: string) {
+    return [`"${message}"`];
+}
+
+export function cssBlue(message: string) {
+    return [`.${message}`];
+}
+
+export function asciiBlue(message: string) {
+    return [`= ${message} =`];
+}
+
+export function blueLine(message: string) {
+    return ['```ini', `[${message}]`, '```'];
+}
+
+export function redLine(message: string) {
+    return ['```css', `[${message}]`, '```'];
+}
+
+export function greenLine(message: string) {
+    return ['```bash', `"${message}"`, '```'];
+}
+
+
+export function formatCss(lines: string[]) {
+    return ['```css', ...lines, '```'];
+}
+
+export function formatAscii(lines: string[]) {
+    return ['```asciidoc', ...lines, '```'];
+}
